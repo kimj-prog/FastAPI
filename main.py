e49d139b994c6b3d3c7506a5cd41e1193749dfef
Jonathan Kim <jkim@jonathansmbp84.mynetworksettings.com>
2026-01-11 22:04:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,16 +13,18 @@ items = []
 def root():
     return {"Hello": "World"}
 
+##adds items to the items list 
 @app.post("/items")
 def create_item(item: Item):
     items.append(item)
     return items
 
+## returns list of items up to 10 elements
 @app.get("/items", response_model=list[Item])
 def list_items(limit: int = 10):
     return items[0:limit]
 
-
+## gets the item at the specified index, if no item exists at that idnex 404 status code is used to show the user the item is not found instead of printing out a confusing error
 @app.get("/items/{item_id}", response_model=Item)
 def get_item(item_id: int) -> Item:
     if item_id < len(items):
